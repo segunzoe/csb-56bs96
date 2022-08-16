d15cd1ea3cfb0064ce706f33baaa87cc423ac0c1
Code Sandbox <hello@codesandbox.io>
2022-08-16 11:16:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4e3d287a686e43a52621fc1365d99a72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32c23ef4a4dbb4f2de4dba2766485e1",
+    "url": "https://segunzoe.github.io/csb-56bs96/index.html"
+  },
+  {
+    "revision": "6a019708122ef2195904",
+    "url": "https://segunzoe.github.io/csb-56bs96/static/js/2.fd03c0c9.chunk.js"
+  },
+  {
+    "revision": "78445c9fa038bf6742ee",
+    "url": "https://segunzoe.github.io/csb-56bs96/static/js/main.5055e789.chunk.js"
+  },
+  {
+    "revision": "ce38ba0e79acfde4ab0a",
+    "url": "https://segunzoe.github.io/csb-56bs96/static/js/runtime-main.b7ffe0c3.js"
+  }
+]);
